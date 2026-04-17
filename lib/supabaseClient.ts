67afa96b13cbec6b41be76aee7886d3ccc006be1
aidanhudd <aidanhudd@gmail.com>
2026-04-17 11:55:49 -0400

    Delete lib directory

--- lib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://trvobbrhkydjxilpiwzv.supabase.co'
-const supabaseAnonKey = 'sb_publishable__x1WFFbaZmAEZF5GrOB4Gg_0X20jv65'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
